bdd5a786960b49dd3b048bb63ce47a7921bb4f76
josvictorcs <victorcassiano7@gmail.com>
2025-07-07 21:14:18 -0300

    agora vai

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<HomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tarefas Médicas"),
+        title: Text("--Tarefas Médicas--"),
         actions: [
           IconButton(
             icon: Icon(_dark ? Icons.dark_mode: Icons.light_mode),
